449a1e1f8fff7a1c5ac0d594718880bf68d0f32b
yuzz2134 <yuzz2134@gmail.com>
2022-06-07 15:37:27 +0900

    Initial commit

--- src/main/java/com/sbaws/book/springboot/domain/user/User.java
+++ src/main/java/com/sbaws/book/springboot/domain/user/User.java
@@ -12,6 +12,8 @@ import javax.persistence.*;
 @Entity
 public class User extends BaseTimeEntity {
 
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long Id;
@@ -38,6 +40,8 @@ public class User extends BaseTimeEntity {
         this.picture = picture;
         this.role = role;
     }
+
+    public User() {}
 
     public User update(String name, String picture){
         this.name = name;
